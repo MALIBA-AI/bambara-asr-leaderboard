0bf3f6018afffa14ca1c2b08413cd4c75aa0a39e
HF Space <sudoping01@gmail.com>
2025-11-24 12:23:12 +0000

    fix: fic huggingface token name

--- space/settings/models.py
+++ space/settings/models.py
@@ -3,12 +3,12 @@ from pydantic_settings import BaseSettings
 import os
 
 class Settings(BaseSettings):
-    dataset_url: str      = Field(validation_alias="DATASET_URL", default="sudoping01/bam-asr-benchmark")
+    dataset_url: str      = Field(validation_alias="DATASET_URL", default="sudoping01/bam-asr-benchmark") # this is where we have the label this is supposed to not be open (closed)
     dataset_config: str   = Field(validation_alias="DATASET_CONFIG", default="default")
     split_name: str       = Field(validation_alias="SPLIT_NAME", default="eval")
-    leaderboard_file: str = Field(validation_alias="LEADERBOARD_FILE", default="leaderboard.csv")
+    leaderboard_file: str = Field(validation_alias="LEADERBOARD_FILE", default="record/leaderboard.csv")
     logo_path: str        = Field(validation_alias="LOGO_PATH", default="assets/images/bambara-logo.png")
-    hf_token: str         = Field(validation_alias="HF_TOKEN", default=os.environ.get("HF_TOKEN"))
+    hf_token: str         = Field(validation_alias="HF_TOKEN", default=os.environ.get("HG_TOKEN"))
     github_user: str      = Field(validation_alias="GITHUB_USER", default="sudoping01")
     github_repo: str      = Field(validation_alias="GITHUB_REPO", default="MALIBA-AI/bambara-asr-leaderboard")
     github_email: str     = Field(validation_alias="GITHUB_EMAIL", default="sudoping01@gmail.com")
