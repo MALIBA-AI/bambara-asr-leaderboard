2caa9aaee7136ef0a6ca3a9f75d314a6c5282717
HF Space <sudoping01@gmail.com>
2025-11-25 10:29:43 +0000

    chlore: change the record file path

--- space/settings/models.py
+++ space/settings/models.py
@@ -6,7 +6,7 @@ class Settings(BaseSettings):
     dataset_url: str      = Field(validation_alias="DATASET_URL", default="sudoping01/bam-asr-benchmark") # this is where we have the label this is supposed to not be open (closed)
     dataset_config: str   = Field(validation_alias="DATASET_CONFIG", default="default")
     split_name: str       = Field(validation_alias="SPLIT_NAME", default="eval")
-    leaderboard_file: str = Field(validation_alias="LEADERBOARD_FILE", default="record/leaderboard.csv")
+    leaderboard_file: str = Field(validation_alias="LEADERBOARD_FILE", default="leaderboard.csv")
     logo_path: str        = Field(validation_alias="LOGO_PATH", default="assets/images/bambara-logo.png")
     hf_token: str         = Field(validation_alias="HF_TOKEN", default=os.environ.get("HG_TOKEN"))
     github_user: str      = Field(validation_alias="GITHUB_USER", default="sudoping01")
